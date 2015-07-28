d26d68e01254f830224510fdbf3cfb36691cd348
Chris Caragianis <chriscaragianis@gmail.com>
2015-07-27 22:04:29 -0400

    Refactored todo list create spec

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -5,27 +5,28 @@ RSpec.configure do |c|
 require 'spec_helper'
 
 describe "Creating todo lists" do 
-  it "redirects to the todo list index page on success" do
+  def create_todo_list(options={})
+    options [:title] ||= "My Todo List"
+    options [:description] ||= "This is what I'm doing today"
+    
     visit "/todo_lists"
     click_link "New Todo list"
     expect(page).to have_content("New Todo List")
     
-    fill_in "Title", with: "My to do list"
-    fill_in "Description", with: "This is what I'm doing today"
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
     click_button "Create Todo list"
-    expect(page).to have_content("My to do list")
+  end
+
+  it "redirects to the todo list index page on success" do
+    create_todo_list     
+    expect(page).to have_content("My Todo List")
   end
 
   it "displays an error when to do list has no title" do 
     expect(TodoList.count()).to eq(0)  
 
-    visit "/todo_lists"
-    click_link "New Todo list"
-    expect(page).to have_content("New Todo List")
-
-    fill_in "Title", with: ""
-    fill_in "Description", with: "This is what I'm doing today"
-    click_button "Create Todo list"
+    create_todo_list title: ""
     expect(page).to have_content("error")
     expect(TodoList.count()).to eq(0)  
     
@@ -36,13 +37,7 @@ describe "Creating todo lists" do
   it "displays an error when to do title has less than three characters" do
     expect(TodoList.count()).to eq(0)  
 
-    visit "/todo_lists"
-    click_link "New Todo list"
-    expect(page).to have_content("New Todo List")
-    fill_in "Title", with: "Hi"
-
-    fill_in "Description", with: "This is what I'm doing today"
-    click_button "Create Todo list"
+    create_todo_list title: "hi"
     expect(page).to have_content("error")
     expect(TodoList.count()).to eq(0)  
     
